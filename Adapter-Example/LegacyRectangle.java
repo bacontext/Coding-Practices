64f235709e7a6b8ccc3683af49d7c08ed0c3cedc
Noel Kim <balaya.kim@gmail.com>
2016-01-05 22:34:03 -0500

    updated adapter pattern

---
+++ Adapter-Example/LegacyRectangle.java
@@ -0,0 +1,6 @@
+public class LegacyRectangle{
+
+	public void display(float x1, float y1, float x2, float x2){
+		// display the rectangle
+	}
+}
--- Adapter-Example/Rectangle.java
+++ Adapter-Example/Rectangle.java
@@ -1,3 +1,9 @@
-public class Rectangle{
+public class Rectangle implements Shape {
+	
+	private LegacyRectangle rect = new LegacyRectangle();
+
+	public void display(int x1,int y1, int x2, int y2){
+		// display the rectangle
+	}
 
 }
