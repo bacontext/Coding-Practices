eb6faa951912e30d3bd9c74e8af1b159c0444ae4
kimcodes <kimcodes@gmail.com>
2016-10-15 21:02:47 -0400

    coded stack class

--- Data-Structures/Stack/CustomStack.java
+++ Data-Structures/Stack/CustomStack.java
@@ -1,7 +1,42 @@
 class CustomStack {
   Node top;
+  int size;
 
   public CustomStack(){
-
+    top = new Node(null);
+    size = 0;
   }
-}
+
+  public void push(Object data){
+    Node tempNode = new Node(data);
+    if(top.getData()==null){
+      top = node;
+    } else{
+      tempNode = tempNode.next(top);
+      top = tempNode;
+    }
+    size++;
+  }
+
+  public Node pop(){
+    // make sure list not empty
+    // check size = 1
+    Node tempNode = new Node(top.data);
+    top = top.next();
+    size--;
+
+    return tempNode;
+  }
+
+  /**
+   * a.k.a peek
+   */
+  public Node top(){
+    return new Node(top.data);
+  }
+
+  public boolean isEmpty(){
+    return size==0;
+  }
+
+} // end of stack class
